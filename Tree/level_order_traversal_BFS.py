2f3904b7fd3aa86e1ec4febdecb89cb271d00c29
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-11-17 11:20:32 +0600

    Level order traversal - BFS

---
+++ Tree/level_order_traversal_BFS.py
@@ -0,0 +1,39 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+root = TreeNode('R')
+nodeA = TreeNode('A')
+nodeB = TreeNode('B')
+nodeC = TreeNode('C')
+nodeD = TreeNode('D')
+nodeE = TreeNode('E')
+nodeF = TreeNode('F')
+nodeG = TreeNode('G')
+
+root.left = nodeA
+root.right = nodeB
+nodeA.left = nodeC
+nodeA.right = nodeD
+nodeB.left = nodeE
+nodeB.right = nodeF
+nodeF.left = nodeG
+
+
+# Level Order Traversal
+def bfs(root):
+    if root is None:
+        return 
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        print(node.data, end=" ")
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    print("")
+bfs(root)
